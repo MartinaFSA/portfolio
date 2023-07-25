3ed762a2c69900440d2f31ac1e09580bd580d556
Martina Fernandez <martinafsa8@gmail.com>
2023-07-25 14:34:44 -0300

    test changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/portfolio/',
   plugins: [
@@ -17,7 +16,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: './index.html'
+        main: resolve(__dirname, 'index.html')
       },
       external: ['vue'],
       output: {
